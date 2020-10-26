cfcf41c1c5ac0fabe4db7f5939796446e6d45827
Furcht968 <tomu.iwanaga129+github@gmail.com>
2020-10-27 05:53:56 +0900

    ソースコードの最適化

--- Virtual2038.py
+++ Virtual2038.py
@@ -1,10 +1,8 @@
 import time
 from datetime import datetime
-import os
 import pygame
 import sys
 from pygame.locals import *
-
 if len(sys.argv)==1:
 	bit=32
 else:
@@ -12,33 +10,33 @@ else:
 if len(sys.argv)<=2:
 	backcolor=(255,255,255)
 	textcolor=(0,0,0)
-	modetext=None
+	dark=False
 else:
 	if(sys.argv[2]=="-dark" or sys.argv[2]=="-d"):
 		backcolor=(20,20,20)
 		textcolor=(255,255,255)
-		modetext="Dark"
+		dark=True
 
 
 for i in range(bit):
 	bitover=2**int(i)
 
-bitover=bitover+bitover
+bitover=bitover*2
 
 pygame.init()
 screen = pygame.display.set_mode((640, 360))
 
 if(bit!=32):
-	if(modetext!=None):
-		pygame.display.set_caption("Virtual2038 on "+str(bit)+"bit, "+modetext+" mode")
+	if(dark!=False):
+		pygame.display.set_caption(f"Virtual2038 on {bit}bit, Dark mode")
 	else:
-		pygame.display.set_caption("Virtual2038 on "+str(bit)+"bit")
+		pygame.display.set_caption(f"Virtual2038 on {bit}bit")
 
 else:
-	if(modetext!=None):
-		pygame.display.set_caption("Virtual2038 on Default "+str(bit)+"bit. "+modetext+" mode")
+	if(dark!=False):
+		pygame.display.set_caption(f"Virtual2038 on Default {bit}bit. Dark mode")
 	else:
-		pygame.display.set_caption("Virtual2038 on Default"+str(bit)+"bit")
+		pygame.display.set_caption(f"Virtual2038 on Default {bit}bit")
 
 font = pygame.font.SysFont(None, 32)
 font2 = pygame.font.SysFont(None, 70)
@@ -48,14 +46,14 @@ while(1):
 	time32u2=format(int(time.time()),"b")[-bit:]
 	time32u10=int(time32u2,2)
 	time32d10=datetime.fromtimestamp(time32u10)
-	bin = font.render("Binary: "+time32u2, True, textcolor)
-	dec = font.render("Decimal: "+str(time32u10), True, textcolor)
+	bin = font.render(f"Binary: {time32u2}", True, textcolor)
+	dec = font.render(f"Decimal: {time32u10}", True, textcolor)
 	txt = font2.render("RealTime:", True, textcolor)
 	utc = font2.render(str(time32d10).replace("-","/"), True, textcolor)
 	if(bitover-time32u10<=10):
-		ovr = font.render("Until Overflow: "+str(bitover-time32u10), True, (255,0,0))
+		ovr = font.render(f"Until Overflow: {bitover-time32u10}", True, (255,0,0))
 	else:
-		ovr = font.render("Until Overflow: "+str(bitover-time32u10), True, textcolor)
+		ovr = font.render(f"Until Overflow: {bitover-time32u10}", True, textcolor)
 	screen.blit(bin, (90,60))
 	screen.blit(dec, (90,80))
 	screen.blit(txt, (90,140))
---
+++ push.bat
@@ -0,0 +1,4 @@
+git remote add origin https://github.com/Furcht968/Virtual2038.git
+git add .
+git commit -m %1
+git push origin master
