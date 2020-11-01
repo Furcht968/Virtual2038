5258807986b6a104493cf56f1b80357d341b8969
Furcht968 <tomu.iwanaga129+github@gmail.com>
2020-11-01 10:28:15 +0900

    大型アップデート！モジュールとしてインポートすることによって別のPythonスクリプトから取得できるようになりました。詳細はdocs/patchnote.mdにて！

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 @echo off
 git add .
 git commit -m %1
-git branch -M main
+git branch -M master
 git remote add origin https://github.com/Furcht968/Virtual2038.git
-git push -u origin main
+git push -u origin master
