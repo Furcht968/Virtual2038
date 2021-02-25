2c1e0abcf763b5e1023690d99334b9ba74f9d3a6
Furcht968 <tomu.iwanaga129+github@gmail.com>
2021-02-25 11:00:45 +0900

    ch: ビルド設定, config.jsonミスってたぁぁぁぁぁぁぁぁぁぁ！！！！！

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import subprocess
 import shutil
 import os
 
-subprocess.run("python3 -m PyInstaller --noconsole --onefile --icon=icon.ico --version-file meta_file/VersionInfoFile.txt Virtual2038.py", shell=True)
-subprocess.run("python3 -m PyInstaller --noconsole --onefile --icon=icon.ico --version-file meta_file/VersionInfoFile_Config.txt Virtual2038_Config.py", shell=True)
+subprocess.run("python3 -m PyInstaller --noconsole --onefile --icon=icons/icon.ico --version-file meta_file/VersionInfoFile.txt Virtual2038.py", shell=True)
+subprocess.run("python3 -m PyInstaller --noconsole --onefile --icon=icons/icon.ico --version-file meta_file/VersionInfoFile_Config.txt Virtual2038_Config.py", shell=True)
 if(os.path.exists("./dist/icons/") == True):
     shutil.rmtree("./dist/icons/")
 shutil.copytree("./icons/", "./dist/icons/")
